b28c48ed06957d5c35876d18d73b04154807c553
Bajcur <130090609+Bajcur@users.noreply.github.com>
2023-10-05 20:16:42 +0200

    Change of the script file
    Previously wrong script file got uploaded into the main branch. This commit fixes this error.

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
-"use strict"
+'use strict'
 
-/*
+const myLibrary = [];
 
-*/
-// Initialize constructor functions
-function Hero(name, level) {
-    this.name = name;
-    this.level = level;
-  }
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        if (read === true){
+            return `${title} by ${author}, ${pages} pages, read.`
+        } else {
+            return `${title} by ${author}, ${pages} pages, not read yet.`
+        }
+    }
+}
 
-  Hero.prototype.greet = function () {
-    return `${this.name} says hello.`;
-  }
-  
-  function Warrior(name, level, weapon) {
-    Hero.call(this, name, level);
-  
-    this.weapon = weapon;
-  }
-  
-  function Healer(name, level, spell) {
-    Hero.call(this, name, level);
-  
-    this.spell = spell;
-  }
-  
-  // Link prototypes and add prototype methods
-  Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
-  Object.setPrototypeOf(Healer.prototype, Hero.prototype);
-  
-  Warrior.prototype.attack = function () {
-    return `${this.name} attacks with the ${this.weapon}.`;
-  }
-  
-  Healer.prototype.heal = function () {
-    return `${this.name} casts ${this.spell}.`;
-  }
-  
-  // Initialize individual character instances
-  const hero1 = new Warrior('Bjorn', 1, 'axe');
-  const hero2 = new Healer('Kanin', 1, 'cure');
-  const hero3 = new Hero("Koks", 2, "chuj");
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
+const Dune = new Book("Dune", "Frank Herbert", 295, true);
+console.log(theHobbit);
 
+function addBookToLibrary(book) {
+    return myLibrary.push(book);
+}
 
-  console.log(hero1);
-  console.log(hero3.greet());
-  console.log(hero2.greet());
+function displayLibrary() {
+    myLibrary.forEach(Book => {
+        let div = document.createElement("div");
+        let ul = document.createElement("ul");
+        div.classList.add("card");
+        ul.classList.add("card-list");
+        div.id = Book.title;
+        ul.id = `${Book.title}-list`;
+        document.body.appendChild(div);
+        document.getElementById(Book.title).appendChild(ul);
+        for (const key in Book) {
+            if (key != "info"){
+                let li = document.createElement("li");
+                li.innerHTML = Book[key];
+                document.getElementById(`${Book.title}-list`).appendChild(li);
+            }
+        }
+    });
+}
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(Dune);
+displayLibrary();
